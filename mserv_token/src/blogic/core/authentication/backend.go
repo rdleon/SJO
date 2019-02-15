a9ce34407da48b817f82fb0b7d449ddcf726f6f7
Edward Nygma <j4nusx@yahoo.com>
2019-02-15 11:38:47 -0800

    US000: backend authentication
    Signed-off-by: Edward Nygma <j4nusx@yahoo.com>

---
+++ mserv_token/src/blogic/core/authentication/backend.go
@@ -0,0 +1,24 @@
+package authentication
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+)
+
+type JWTAuthenticationBackend struct {
+	privateKey *rsa.PrivateKey
+	PublicKey  *rsa.PublicKey
+}
+
+var authBackendInstance *JWTAuthenticationBackend = nil
+
+func InitJWTAuthenticationBackend() *JWTAuthenticationBackend {
+	if authBackendInstance == nil {
+		authBackendInstance = &JWTAuthenticationBackend{
+			privateKey: getPrivateKey(),
+			PublicKey:  getPublicKey(),
+		}
+	}
+
+	return authBackendInstance
+}
