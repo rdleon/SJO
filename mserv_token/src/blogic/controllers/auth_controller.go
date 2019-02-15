4b8130fb098bbec843b1b1a55c26a612c7396d4b
Edward Nygma <j4nusx@yahoo.com>
2019-02-15 11:26:09 -0800

    US0000: First controller for authentication
    Signed-off-by: Edward Nygma <j4nusx@yahoo.com>

---
+++ mserv_token/src/blogic/controllers/auth_controller.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"blogic/services"
+	"blogic/services/models"
+	"encoding/json"
+	"net/http"
+)
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	requestUser := new(models.User)
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&requestUser)
+
+	responseStatus, token := services.Login(requestUser)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(responseStatus)
+	w.Write(token)
+}
