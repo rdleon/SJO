4470b46d1b2fb5e524a7476f8775d060865c1678
Bob Ross <pianodaemon@gmail.com>
2019-02-23 15:13:51 -0600

    US000: Logout sso
    Signed-off-by: Bob Ross <pianodaemon@gmail.com>

--- sso/service/src/blogic/core/authentication/backend.go
+++ sso/service/src/blogic/core/authentication/backend.go
@@ -22,3 +22,21 @@ func InitJWTAuthenticationBackend() *JWTAuthenticationBackend {
 
 	return authBackendInstance
 }
+
+func (backend *JWTAuthenticationBackend) GenerateToken(userUUID string) (string, error) {
+	token := jwt.New(jwt.SigningMethodRS512)
+	token.Claims["exp"] = time.Now().Add(time.Hour * time.Duration(settings.Get().JWTExpirationDelta)).Unix()
+	token.Claims["iat"] = time.Now().Unix()
+	token.Claims["sub"] = userUUID
+	tokenString, err := token.SignedString(backend.privateKey)
+	if err != nil {
+		panic(err)
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func (backend *JWTAuthenticationBackend) Logout(tokenString string, token *jwt.Token) error {
+	redisConn := redis.Connect()
+	return redisConn.SetValue(tokenString, tokenString, backend.getTokenRemainingValidity(token.Claims["exp"]))
+}
