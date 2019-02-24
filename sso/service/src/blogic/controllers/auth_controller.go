7e83133396a7cecf08051cbc4b5307194b500f0f
Bob Ross <pianodaemon@gmail.com>
2019-02-23 18:58:07 -0600

    US000: Logout and refresh in controllers
    Signed-off-by: Bob Ross <pianodaemon@gmail.com>

--- sso/service/src/blogic/controllers/auth_controller.go
+++ sso/service/src/blogic/controllers/auth_controller.go
@@ -17,3 +17,22 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(responseStatus)
 	w.Write(token)
 }
+
+func RefreshToken(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	requestUser := new(models.User)
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&requestUser)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(services.RefreshToken(requestUser))
+}
+
+func Logout(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	err := services.Logout(r)
+	w.Header().Set("Content-Type", "application/json")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	} else {
+		w.WriteHeader(http.StatusOK)
+	}
+}
