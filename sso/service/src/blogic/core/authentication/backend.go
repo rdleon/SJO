17321c2f077a0e7f05c4421d0e2a33ae49268420
Bob Ross <pianodaemon@gmail.com>
2019-02-23 18:21:10 -0600

    US000: User on sso until now has been hardcoded
    Signed-off-by: Bob Ross <pianodaemon@gmail.com>

--- sso/service/src/blogic/core/authentication/backend.go
+++ sso/service/src/blogic/core/authentication/backend.go
@@ -4,8 +4,21 @@ import (
 	"api.jwt.auth/core/redis"
 	"api.jwt.auth/services/models"
 	"api.jwt.auth/settings"
+	"golang.org/x/crypto/bcrypt"
 	jwt "github.com/dgrijalva/jwt-go"
 )
+
+func (backend *JWTAuthenticationBackend) Authenticate(user *models.User) bool {
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte("testing"), 10)
+
+	testUser := models.User{
+		UUID:     "a6f17a00-61b4-4281-a2ac-655034093bb2",
+		Username: "mazinger",
+		Password: string(hashedPassword),
+	}
+
+	return user.Username == testUser.Username && bcrypt.CompareHashAndPassword([]byte(testUser.Password), []byte(user.Password)) == nil
+}
 
 func (backend *JWTAuthenticationBackend) getTokenRemainingValidity(timestamp interface{}) int {
 	if validity, ok := timestamp.(float64); ok {
