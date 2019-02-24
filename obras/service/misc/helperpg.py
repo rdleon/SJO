3008717cdb4d2d42d410d83a82a6f1c7ebae8ecc
Bob Ross <pianodaemon@gmail.com>
2019-02-23 19:21:13 -0600

    US000: Pgsql connection
    Signed-off-by: Bob Ross <pianodaemon@gmail.com>

---
+++ obras/service/misc/helperpg.py
@@ -0,0 +1,22 @@
+import psycopg2
+import psycopg2.extras
+from .common import env_property
+
+
+class HelperPg(object):
+    """
+    """
+
+    @staticmethod
+    def connect():
+        """opens a connection to database"""
+
+        # order here matters
+        env_vars = ['MS_DBMS_DB', 'MS_DBMS_USER', 'MS_DBMS_HOST', 'MS_DBMS_PASS', 'MS_DBMS_PORT']
+        t = tuple(map(env_property, env_vars))
+
+        try:
+            conn_str = "dbname={0} user={1} host={2} password={3} port={4}".format(*t)
+            return psycopg2.connect(conn_str)
+        except:
+            raise Exception('It is not possible to connect with database')
