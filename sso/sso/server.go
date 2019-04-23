491a63b83150a3749943126038f480af0663e1a6
R. Diaz de Leon <leon@elinter.net>
2019-04-23 00:22:09 -0500

    Simple logging for SSO server
    Added logging for the start of the server and reading the port
    from the ENV variable
    
    pianodaemon/SJO#26

--- sso/sso/server.go
+++ sso/sso/server.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/codegangsta/negroni"
+
 	"sso/routers"
 	"sso/settings"
-	"github.com/codegangsta/negroni"
-	"net/http"
 )
 
 func main() {
+	var defaultAddr = "0.0.0.0"
+
 	settings.Init()
+
 	router := routers.InitRoutes()
 	n := negroni.Classic()
 	n.UseHandler(router)
-	http.ListenAndServe(":8080", n)
+
+	port := os.Getenv("PORT")
+
+	if port != "" {
+		fmt.Fprintf(os.Stderr, "Env var $PORT is not set, exiting...")
+		os.Exit(1)
+	}
+
+	addr := fmt.Sprintf("%s:%s", defaultAddr, port)
+
+	fmt.Printf("Listening on %s...\n", addr)
+
+	http.ListenAndServe(addr, n)
 }
