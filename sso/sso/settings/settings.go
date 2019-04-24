a1a772984121f2b9ade382aaf129804eee2065fd
R. Diaz de Leon <leon@elinter.net>
2019-04-23 20:40:18 -0500

    Sharing database info between containers
    Share the database connection info between the web/sso
    container and the PostgreSQL database container.
    
    pianodaemon/SJO#40

--- sso/sso/settings/settings.go
+++ sso/sso/settings/settings.go
@@ -8,6 +8,11 @@ import (
 type Settings struct {
 	PrivateKeyPath     string
 	PublicKeyPath      string
+	DatabaseName       string
+	DatabaseHost       string
+	DatabaseUser       string
+	DatabasePassword   string
+	DatabasePort       int
 	JWTExpirationDelta int
 }
 
@@ -16,9 +21,16 @@ var settings Settings = Settings{}
 func Init() {
 	// This is error prone...
 	delta, _ := strconv.Atoi(os.Getenv("EXP_DELTA"))
+	db_port, _ := strconv.Atoi(os.Getenv("DB_PORT"))
+
 	settings = Settings{
 		PrivateKeyPath:     os.Getenv("PRIVATE_KEY"),
 		PublicKeyPath:      os.Getenv("PUBLIC_KEY"),
+		DatabaseName:       os.Getenv("POSTGRES_DB"),
+		DatabaseHost:       os.Getenv("POSTGRES_HOST"),
+		DatabaseUser:       os.Getenv("POSTGRES_USER"),
+		DatabasePassword:   os.Getenv("POSTGRES_PASSWORD"),
+		DatabasePort:       db_port,
 		JWTExpirationDelta: delta,
 	}
 }
