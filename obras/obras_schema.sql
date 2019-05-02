d0d7addc3064b2c72c530c85c67767b922f14563
Bob Ross <pianodaemon@gmail.com>
2019-05-02 16:36:48 -0700

    Fixed typo

--- obras/obras_schema.sql
+++ obras/obras_schema.sql
@@ -26,7 +26,7 @@ ALTER TABLE ONLY contracts
 ALTER TABLE ONLY contracts
     ADD CONSTRAINT contract_unique_title UNIQUE (title);
 
-COMMENT ON TABLE  contracts IS 'Relacion que alberga los contratos vinculados a un proyecto';
+COMMENT ON TABLE  contracts IS 'Relacion que alberga los contratos';
 COMMENT ON COLUMN contracts.title IS 'Nombre con el que se identifica a este contrato';
 COMMENT ON COLUMN contracts.inception_time IS 'Fecha en la que se registro este contrato';
 COMMENT ON COLUMN contracts.touch_latter_time IS 'Apunta a la ultima fecha de alteracion de el registro';
