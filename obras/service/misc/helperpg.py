115e48db3eb45d6222bf2adcabe460acaedd401e
Edward Nygma <j4nusx@yahoo.com>
2019-05-05 12:58:12 -0700

    pgsql python decorator for obras
    The advent of a fancy pgsql decorator allows us to safety handle
    connections and disconnection to postgresql famiy engines from eight to eleven.
    
    At least I think so :)
    
    Signed-off-by: Edward Nygma <j4nusx@yahoo.com>

--- obras/service/misc/helperpg.py
+++ obras/service/misc/helperpg.py
@@ -3,20 +3,43 @@ import psycopg2.extras
 from .common import env_property
 
 
-class HelperPg(object):
-    """
-    """
+def _connect():
+    """Opens a connection to database"""
+    # order here matters
+    env_vars = ('DBMS_SCHEMA', 'DBMS_USER', 'DBMS_HOST', 'DBMS_PASS', 'DBMS_PORT')
+    t = tuple(map(env_property, env_vars))
+    try:
+        conn_str = "dbname={0} user={1} host={2} password={3} port={4}".format(*t)
+        return psycopg2.connect(conn_str)
+    except:
+        raise Exception('It is not possible to connect with database')
 
-    @staticmethod
-    def connect():
-        """opens a connection to database"""
 
-        # order here matters
-        env_vars = ('DBMS_SCHEMA', 'DBMS_USER', 'DBMS_HOST', 'DBMS_PASS', 'DBMS_PORT')
-        t = tuple(map(env_property, env_vars))
+def _query(conn, sql, commit=False):
+    """Carries an sql query out to database"""
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute(sql)
+    if commit:
+        conn.commit()
+    rows = cur.fetchall()
+    cur.close()
+    if len(rows) > 0:
+        return rows
+    # We should not have reached this point
+    raise Exception('There is not data retrieved')
 
+
+def pgsql(func):
+    """Handy decorator to fetch a database connection"""
+    def wrapper(q):
+        c = _connect()
         try:
-            conn_str = "dbname={0} user={1} host={2} password={3} port={4}".format(*t)
-            return psycopg2.connect(conn_str)
+            return func(c, q)
         except:
-            raise Exception('It is not possible to connect with database')
+            raise
+        finally:
+            conn.close()
+    return wrapper
+
+
+
