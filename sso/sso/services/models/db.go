e573ba203db6505168617a6642f922a0b57560ec
Rafael Diaz de Leon <leon@elinter.net>
2019-05-07 20:44:24 -0500

    Database connection handler
    pianodaemon/SJO#40

---
+++ sso/sso/services/models/db.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+
+	"sso/settings"
+)
+
+var DB *sql.DB
+
+func getConnStr() string {
+	var config = settings.Get()
+
+	// SSL mode disable to use in containers
+	return fmt.Sprintf("user=%s password=%s host=%s port=%d dbname=%s sslmode=disable",
+		config.DatabaseUser,
+		config.DatabasePassword,
+		config.DatabaseHost,
+		config.DatabasePort,
+		config.DatabaseName)
+}
+
+func InitDB() (*sql.DB, error) {
+	var err error
+
+	connStr := getConnStr()
+
+	DB, err = sql.Open("postgres", connStr)
+
+	return DB, err
+}
