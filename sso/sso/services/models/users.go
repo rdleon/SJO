baf34d46b27b211d7dbf5338e03b2b9ae4396e98
Rafael Diaz de Leon <leon@elinter.net>
2019-05-07 21:45:07 -0500

    User database functions
    Database functions to use with the User model:
    
      * Get a user by uuid
      * Create a new user given name and password
      * Update a user's password
    
    pianodaemon/SJO#40

--- sso/sso/services/models/users.go
+++ sso/sso/services/models/users.go
@@ -1,7 +1,120 @@
 package models
 
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
-	UUID     string `json:"uuid" form:"-"`
-	Username string `json:"username" form:"username"`
-	Password string `json:"password" form:"password"`
+	UUID      string `json:"uuid" form:"-"`
+	Username  string `json:"username" form:"username"`
+	IsActive  bool   `json:"isActive" form:"is_active"`
+	CreatedAt int64  `json:"createdAt" form:"created_at"`
+}
+
+func ValidatePassword(password string) error {
+	if len(password) < 4 {
+		return errors.New("Password too short")
+	}
+
+	return nil
+}
+
+func LoadUser(uuid string) *User {
+	var (
+		username  string
+		createdAt int64
+		isActive  bool
+	)
+
+	db := GetDB()
+
+	err := db.QueryRow("SELECT username, is_active, craeted_at FROM users WHERE uuid = $1;", uuid).Scan(&username, &isActive, &createdAt)
+
+	if err != nil {
+		return nil
+	}
+
+	return &User{
+		UUID:      uuid,
+		Username:  username,
+		IsActive:  isActive,
+		CreatedAt: createdAt,
+	}
+}
+
+func (user *User) UpdatePassword(password string) error {
+	err := ValidatePassword(password)
+
+	if err != nil {
+		return err
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return err
+	}
+
+	db := GetDB()
+
+	err = db.QueryRow("UPDATE users SET password = '$1' WHERE uuid = $2", string(hash), user.UUID).Scan()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func CreateUser(username string, password string) (*User, error) {
+	var uuid string
+
+	err := ValidatePassword(password)
+
+	if err != nil {
+		return nil, err
+	}
+
+	db := GetDB()
+
+	err = db.QueryRow("INSERT INTO users(username) VALUES('$1')", username).Scan(&uuid)
+
+	if err != nil {
+		return nil, err
+	}
+
+	user := LoadUser(uuid)
+
+	if user == nil {
+		return nil, errors.New("Error loading the new user")
+	}
+
+	err = user.UpdatePassword(password)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func ValidLogin(username string, password string) bool {
+	var passwordHash string
+
+	db := GetDB()
+
+	err := db.QueryRow("SELECT password FROM users WHERE username = $1", username).Scan(&passwordHash)
+
+	if err != nil {
+		return false
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(password))
+
+	if err != nil {
+		return false
+	}
+
+	return true
 }
