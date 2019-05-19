ed8e52c38e9e9310c63cb91fe7f5bf88a1021b0c
Edward Nygma <j4nusx@yahoo.com>
2019-05-19 00:57:44 -0700

    OBRAS: Bare bones of api obras version 1
    Signed-off-by: Edward Nygma <j4nusx@yahoo.com>

--- obras/service/main.py
+++ obras/service/main.py
@@ -1,16 +1,28 @@
 import logging
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, Blueprint
+from genl.restplus import api
+from genl.endpoints import providers
 
-@app.route("/")
-def hello():
-    app.logger.debug('HELLO!!!!!!!!!!')
-    return "<h1 style='color:blue'>Hello There!</h1>"
+
+def setup_app(flask_app):
+    """Setup flask app instance"""
+    blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
+    api.init_app(blueprint)
+
+    api.add_namespace(providers.ns)
+
+    flask_app.register_blueprint(blueprint)
+
+
+app = Flask(__name__)
+setup_app(app)
+
 
 if __name__ == "__main__":
+    # For the sake of faster development
     app.run(host='0.0.0.0')
 else:
+    # On production It is needed for WSGI
     gunicorn_logger = logging.getLogger("gunicorn.error")
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
-
