077a42d9b8870e6ab3a84e915a3fb7489e12714d
Edward Nygma <j4nusx@yahoo.com>
2019-05-19 00:35:14 -0700

    OBRAS: First approach endpoint providers
    Signed-off-by: Edward Nygma <j4nusx@yahoo.com>

---
+++ obras/service/genl/endpoints/providers.py
@@ -0,0 +1,39 @@
+import dal.studs
+from flask import request
+from flask_restplus import Resource
+from genl.restplus import api
+
+
+ns = api.namespace('providers',
+                    description='Operations related to providers')
+
+
+@ns.route('/<int:provider_id>')
+@api.response(404, 'Provider not found.')
+class ProviderItem(Resource):
+
+    def get(self, provider_id):
+        """
+        Returns a provider.
+        """
+        entity = dal.studs.find_provider(provider_id)
+        return entity
+
+    @api.response(204, 'Provider successfully updated.')
+    def put(self, provider_id):
+        """
+        Updates a provider.
+        """
+        req = request.data
+        dic_req = json.loads(req)
+        dic_req['provider_id'] = provider_id
+        dal.studs.edit_provider(**dic_req)
+        return None, 204
+
+    @api.response(204, 'Post successfully deleted.')
+    def delete(self, provider_id):
+        """
+        Deletes a provider.
+        """
+        dal.studs.block_provider(provider_id)
+        return None, 204
