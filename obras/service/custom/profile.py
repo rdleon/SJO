da0d14ed7c6a29bfac9bc3a76085039539fe2346
Edward Nygma <j4nusx@yahoo.com>
2019-05-19 00:44:56 -0700

    OBRAS: Added microservice description
    It is useful for flask api documentation
    
    Signed-off-by: Edward Nygma <j4nusx@yahoo.com>

--- obras/service/custom/profile.py
+++ obras/service/custom/profile.py
@@ -8,4 +8,5 @@ _LOCAL_RESOURCE_DIR = os.path.join(GLOBAL_RESOURCES_DIR, 'local')
 
 
 MSERV_NAME = 'SJO'
+MSERV_DESC = 'Microservicio API Obras'
 MSERV_RESOURCES_DIR = os.path.join(_LOCAL_RESOURCE_DIR, MSERV_NAME)
