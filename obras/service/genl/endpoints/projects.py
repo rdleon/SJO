e71da681c42df882e959cac94bec3cf29939a079
Rafael Diaz de Leon <leon@elinter.net>
2019-05-24 23:20:07 -0500

    Refactored project to it's own file
    Refactored project DAL functions and added stub for
    marshalling

--- obras/service/genl/endpoints/projects.py
+++ obras/service/genl/endpoints/projects.py
@@ -1,39 +1,40 @@
-import dal.studs
+import json
+
 from flask import request
 from flask_restplus import Resource
+
+import dal.project
 from genl.restplus import api
 
-
-ns = api.namespace('projects',
-                    description='Operations related to projects')
+ns = api.namespace("projects", description="Operations related to projects")
 
 
-@ns.route('/<int:project_id>')
-@api.response(404, 'Project not found.')
+@ns.route("/<int:project_id>")
+@api.response(404, "Project not found.")
 class ProjectItem(Resource):
-
     def get(self, project_id):
         """
         Returns a project.
         """
-        entity = dal.studs.find_project(project_id)
+        entity = dal.project.find(project_id)
         return entity
 
-    @api.response(204, 'Project successfully updated.')
+    @api.response(204, "Project successfully updated.")
     def put(self, project_id):
         """
         Updates a project.
         """
         req = request.data
         dic_req = json.loads(req)
-        dic_req['project_id'] = project_id
-        dal.studs.edit_project(**dic_req)
+        dic_req["project_id"] = project_id
+        dal.studs.edit(**dic_req)
+
         return None, 204
 
-    @api.response(204, 'Project successfully deleted.')
+    @api.response(204, "Project successfully deleted.")
     def delete(self, project_id):
         """
         Deletes a project.
         """
-        dal.studs.block_project(project_id)
+        dal.project.block(project_id)
         return None, 204
