4d665f6fc803cbfe42eaed3989076815ac4bc564
Rafael Diaz de Leon <leon@elinter.net>
2019-06-04 23:35:33 -0500

    Fixed schema loading

--- obras/scripts/create_schema.sh
+++ obras/scripts/create_schema.sh
@@ -6,10 +6,10 @@ DATABASE=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@pg:$POSTGRES_PORT
 echo "CREATE DATABASE $POSTGRES_DB "| psql $DATABASE?sslmode=disable
 
 echo "Creating schemas..."
-psql $DATABASE?sslmode=disable < /migrations/schema.sql
+psql $DATABASE/$POSTGRES_DB?sslmode=disable < /migrations/schema.sql
 
 echo "Creating store procedures..."
-psql  $DATABASE?sslmode=disable < /migrations/stores.sql
+psql  $DATABASE/$POSTGRES_DB?sslmode=disable < /migrations/stores.sql
 
 echo "Populating tables..."
-psql  $DATABASE?sslmode=disable < /migrations/populate.sql
+psql  $DATABASE/$POSTGRES_DB?sslmode=disable < /migrations/populate.sql
