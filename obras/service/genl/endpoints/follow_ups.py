2db6817debb330a1c009501d7d7fe4e20db9a6c7
Rafael Diaz de Leon <leon@elinter.net>
2019-06-19 22:49:16 -0500

    Saving images to temp folder

--- obras/service/genl/endpoints/follow_ups.py
+++ obras/service/genl/endpoints/follow_ups.py
@@ -2,7 +2,7 @@ import csv
 import os
 from io import StringIO
 
-from flask import app, json, request
+from flask import json, request
 from flask_restplus import Resource
 
 import dal.follow_ups
@@ -13,20 +13,19 @@ from misc.helperpg import EmptySetError
 ns = api.namespace("follow_ups", description="Operations related to follow_ups")
 
 
-def _save_files(files, follow_up_id=None):
+def _save_files(files, follow_up=None):
     img_paths = []
     paths = None
 
-    if follow_up_id:
-        follow_up = dal.follow_ups.find(follow_up_id)
-        reader = csv.reader(follow_up["img_paths"], delimeter=",")
+    if follow_up:
+        reader = csv.reader(follow_up["img_paths"], delimiter=",")
         for row in reader:
             for path in row:
                 img_paths.append(path)
 
     for key in files:
         if files[key]:
-            filename = os.path.join(app.config["UPLOAD_FOLDER"], files[key].filename)
+            filename = os.path.join("/tmp", files[key].filename)
             files[key].save(filename)
             img_paths.append(filename)
 
@@ -55,8 +54,8 @@ class FollowUpCollection(Resource):
         """
         offset = request.args.get("offset", 0)
         limit = request.args.get("limit", 10)
-        order_by = request.args.get("order_by", "id")
-        order = request.args.get("order", "ASC")
+        order_by = request.args.get("order_by", "check_date")
+        order = request.args.get("order", "DESC")
 
         search_params = get_search_params(
             request.args, ["project", "verified_progress", "check_stage"]
@@ -74,12 +73,6 @@ class FollowUpCollection(Resource):
         Creates a new follow_up.
         """
         follow_up = json.loads(request.data)
-
-        images = request.files.to_dict()
-        img_paths = _save_files(images)
-
-        if img_paths:
-            follow_up["img_paths"] = img_paths
 
         dal.follow_ups.create(**follow_up)
 
@@ -128,12 +121,6 @@ class FollowUpItem(Resource):
         follow_up = json.loads(request.data)
         follow_up["id"] = follow_up_id
 
-        images = request.files.to_dict()
-        img_paths = _save_files(images, follow_up_id)
-
-        if img_paths:
-            follow_up["img_paths"] = img_paths
-
         dal.follow_ups.edit(**follow_up)
 
         return follow_up, 200
@@ -146,3 +133,23 @@ class FollowUpItem(Resource):
         dal.follow_ups.block(follow_up_id)
 
         return None, 204
+
+
+@ns.route("/<int:follow_up_id>/attachment")
+class FollowUpAttachment(Resource):
+    @api.marshal_with(dal.follow_ups.model)
+    def post(self, follow_up_id):
+        """
+        Creates a new attachment to a follow up
+        """
+        follow_up = dal.follow_ups.find(follow_up_id)
+
+        images = request.files.to_dict()
+        img_paths = _save_files(images, follow_up)
+
+        if img_paths:
+            follow_up["img_paths"] = img_paths
+
+        dal.follow_ups.edit(**follow_up)
+
+        return follow_up, 200
